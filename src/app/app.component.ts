c4c2ea1581817cbd0397ec4cea273ac657705e7b
Adrián Maza <adri_mv1@hotmail.com>
2024-05-20 21:59:32 +0200

    models & dynamic attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { GreetingsComponent } from './components/greetings/greetings.component';
+import Product from './models/Product';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, GreetingsComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  personName: string;
+  personAge: number;
+  isFromMadrid: boolean;
+  computer: Product;
+  inputPlaceholder: string;
+
+  constructor() {
+    this.personName = 'Adrián';
+    this.personAge = 33;
+    this.isFromMadrid = false;
+    this.inputPlaceholder = 'Compra este producto';
+
+    this.computer = {
+      id: 1,
+      name: 'Computer',
+      price: 1000,
+      isForSale: true,
+      img: 'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg',
+    };
+  }
 }
