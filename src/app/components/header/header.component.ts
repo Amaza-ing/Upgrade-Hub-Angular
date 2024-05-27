8ff9023dabe2c61f6a55fd37a4892adca2eb9f3d
Adrián Maza <adri_mv1@hotmail.com>
2024-05-27 21:29:20 +0200

    ngClass dark example

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { GreetingsComponent } from '../greetings/greetings.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [GreetingsComponent],
+  imports: [GreetingsComponent, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isDark: boolean;
 
+  constructor() {
+    this.isDark = false;
+  }
+
+  toggleDark(): void {
+    this.isDark = !this.isDark;
+  }
 }
