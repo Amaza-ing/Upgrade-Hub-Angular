81ffd8e085d34b4fde92666f0c89daaee10c0fc8
Adrián Maza <adri_mv1@hotmail.com>
2024-05-30 19:50:17 +0200

    reactive forms movies completed

--- src/app/components/movie-reactive-form/movie-reactive-form.component.ts
+++ src/app/components/movie-reactive-form/movie-reactive-form.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-movie-reactive-form',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './movie-reactive-form.component.html',
-  styleUrl: './movie-reactive-form.component.css'
+  styleUrl: './movie-reactive-form.component.css',
 })
 export class MovieReactiveFormComponent {
   movieForm: FormGroup;
@@ -15,18 +20,22 @@ export class MovieReactiveFormComponent {
   director: FormControl;
 
   constructor() {
-    this.name = new FormControl("", Validators.required);
-    this.duration = new FormControl("", Validators.required);
-    this.director = new FormControl("");
+    this.name = new FormControl('', Validators.required);
+    this.duration = new FormControl('', [
+      Validators.required,
+      Validators.max(300),
+    ]);
+    this.director = new FormControl('');
 
     this.movieForm = new FormGroup({
       name: this.name,
       duration: this.duration,
-      director: this.director
-    })
+      director: this.director,
+    });
   }
 
   handleSubmit(): void {
-    console.log("Movie Created:", this.movieForm.value);    
+    console.log('Movie Created:', this.movieForm.value);
+    this.movieForm.reset();
   }
 }
